a6df2ce0ae73492294043b2563448d6cd002d2eb
Thomas Gautier <thomas.gautier@gmail.com>
2016-08-23 11:12:12 +0200

    Update environment variables with system tuple syntax

--- lib/carselector.ex
+++ lib/carselector.ex
@@ -1,2 +1,16 @@
 defmodule CarSelector do
+  def private_api_key, do: get_env(:private_api_key)
+
+  def url, do: get_env(:url, "https://carselector.groomgroom.co/api")
+
+  def version, do: get_env(:version, 20160719)
+
+  defp get_env(key, default_value \\ nil) do
+    case Application.get_env(:carselector, key, default_value) do
+      {:system, value} ->
+        System.get_env(value)
+      value ->
+        value
+    end
+  end
 end
